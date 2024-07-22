b9d61535ce8558f3d88e911124cb7a2c98851944
welu28 <27luw@millburn.org>
2024-07-22 18:46:17 -0400

    1434

--- poe/poe.java
+++ poe/poe.java
@@ -47,26 +47,97 @@ public class poe {
         scanner.close();
         pw.close();
 
-        sort(words.toArray(new String[0]));
+        String[] arr = words.toArray(new String[0]);
+        sort(arr); // compare the sorting times, but don't actually sort yet (keep unordered for search)
+        
 
         Scanner io = new Scanner(System.in);
         // user input
         System.out.println("Enter a word: ");
         String word = io.next();
+        System.out.println("_______________________________________________");
+        search(arr, word);
+
         long startTime = System.currentTimeMillis();
-        System.out.println("HashMap search started at: " + new Date(startTime));
+        System.out.println("hashmap search started at: " + new Date(startTime));
 
         if(wordCounts.containsKey(word)) {
-            System.out.println("Number of occurences: " + wordCounts.get(word));
+            System.out.println("Word count: " + wordCounts.get(word));
         }
         else {
             System.out.println("WORD NOT IN BOOK");
         }
         
         long endTime = System.currentTimeMillis();
-        System.out.println("HashMap ended at: " + new Date(endTime));
-        System.out.println("HashMap search duration: " + (endTime - startTime) + " milliseconds");
+        System.out.println("hashmap search ended at: " + new Date(endTime));
+        System.out.println("hashmap search duration: " + (endTime - startTime) + " milliseconds");
+        System.out.println("_______________________________________________");
+        System.out.print("check count.txt for more information about word count");
         io.close();
+    }
+
+    public static void search(String[] words, String word) {
+        // make a copy so we don't change the original array
+        String[] copyA = Arrays.copyOf(words, words.length);
+        String[] copyB = Arrays.copyOf(words, words.length);
+
+        Arrays.sort(copyA);
+        System.out.println("binary search:");
+        long start = System.currentTimeMillis();
+        binarySearch(copyA, word);
+        long end = System.currentTimeMillis();
+        System.out.println("binary search time: " + (end - start) + " milliseconds");
+        System.out.println("_______________________________________________");
+
+        System.out.println("sequential search:");
+        start = System.currentTimeMillis();
+        // O(n logn)
+        sequentialSearch(copyB, word);
+        end = System.currentTimeMillis();
+        System.out.println("sequential search time: " + (end - start) + " milliseconds");
+        System.out.println("_______________________________________________");
+    }
+
+    public static void binarySearch(String[] a, String word) {
+        int low = 0;
+        int high = a.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2; // midpoint
+            int comparison = a[mid].compareTo(word);
+            
+            if (comparison == 0) { // found the word
+                int count = 0;
+                while (mid >= 0 && a[mid].equals(word)) {
+                    count++;
+                    mid--;
+                }
+                mid = low + (high - low) / 2 + 1; // reset mid to 1 greater than the initial position
+                while (mid < a.length && a[mid].equals(word)) {
+                    count++;
+                    mid++;
+                }
+                System.out.println("Word count: " + count);
+                return;
+            } else if (comparison < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        System.out.println("WORD NOT IN BOOK");
+    }
+
+    public static void sequentialSearch(String[] a, String word) {
+        int count = 0;
+        for(int i = 0; i < a.length; i++) {
+            count += a[i].equals(word) ? 1 : 0;
+        }
+        if(count == 0) {
+            System.out.println("WORD NOT IN BOOK");
+        }
+        else System.out.println("Word count: " + count);
     }
 
     public static void sort(String[] words) { // compare bubble sort with default sort (merge sort)
@@ -75,14 +146,18 @@ public class poe {
         String[] copyB = Arrays.copyOf(words, words.length);
 
         long start = System.currentTimeMillis();
+        // O(n^2)
         bubbleSort(copyA);
         long end = System.currentTimeMillis();
         System.out.println("bubble sort time: " + (end - start) + " milliseconds");
+        System.out.println("_______________________________________________");
 
         start = System.currentTimeMillis();
+        // O(n logn)
         Arrays.sort(copyB);
         end = System.currentTimeMillis();
         System.out.println("java arrays sort time: " + (end - start) + " milliseconds");
+        System.out.println("_______________________________________________");
     }
 
     /*
